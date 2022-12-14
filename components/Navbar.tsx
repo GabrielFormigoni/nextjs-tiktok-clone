83efd92144f6ea5ced80d6a5cfd97d61b1eafa5e
Gabriel Formigoni <bielformigoni@gmail.com>
2022-12-14 14:49:54 -0300

    :sparkles: feat: Login com conta do google

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,10 @@ import { AiOutlineLogout } from "react-icons/ai"
 import { BiSearch } from "react-icons/bi"
 import { IoMdAdd } from "react-icons/io"
 import logo from "../utils/logo.png"
+
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
@@ -15,6 +18,16 @@ const Navbar = () => {
           <Image className='cursor-pointer' src={logo} alt='Logo' layout='responsive'></Image>
         </div>
       </Link>
+
+      <div>Search</div>
+      {user ? (
+        <div>Logged In</div>
+      ) : (
+        <GoogleLogin 
+          onSuccess={(response) => console.log(response)}
+          onError={() => console.log("Erro!")}
+        />
+      )}
     </div>
   )
 }
